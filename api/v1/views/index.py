483d987a50e5ef38ee6d6ec794c271b941b3cd3b
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-21 00:23:00 -0400

    comments

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,6 @@
 from flask import Flask, jsonify
 from api.v1.views import app_views
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Gives status """
