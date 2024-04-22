e543850d1ad51d93a64a30cdd9c79b0f1a3d995c
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-04-21 22:23:45 -0400

    fixing file_storage testing

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -70,18 +70,16 @@ test_file_storage.py'])
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
-    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def setUp(self):
         """ Set up for the tests """
         self.obj = BaseModel()  # Ensure self.obj is defined
         self.obj.save()
 
-    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_get(self):
         """Test that get returns the correct object"""
         obj = models.storage.get(BaseModel, self.obj.id)
         self.assertEqual(obj, self.obj)
-    
+        
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_all_returns_dict(self):
         """Test that all returns the FileStorage.__objects attr"""
